d291724bd201228ec7c7a85f14de9bb900eded0b
Beau Kinstler <beaukinstler@protonmail.com>
2017-09-17 23:59:53 +0000

    Add customer_search model and views

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'faker'
 gem 'devise'
 gem 'bower-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+
+  get 'customers_controller/index'
+
   devise_for :users
   get 'dashboard/index'
   root 'dashboard#index'
+  resources :customers, only: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
