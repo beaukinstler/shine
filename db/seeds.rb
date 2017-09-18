3b0c5cac09444db39e15af4b1b6adc796559daf8
Beau Kinstler <beaukinstler@protonmail.com>
2017-09-18 01:35:46 +0000

    Add more seeds to the dev db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-1000.times do |i|
+100000.times do |i|
     Customer.create!(
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
