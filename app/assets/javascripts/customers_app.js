e510fbf0bb183c184fef148af04c0691c7262a41
Beau Kinstler <beaukinstler@protonmail.com>
2017-09-24 02:14:11 +0000

    Add initial AngularJS integration

---
+++ app/assets/javascripts/customers_app.js
@@ -0,0 +1,9 @@
+var app = angular.module("customers",[]);
+
+var CustomerSearchController = function($scope){
+    $scope.search = function(searchTerm){
+        $scope.searchedFor = searchTerm;
+    }
+}
+
+app.controller("CustomerSearchController", ["$scope",CustomerSearchController]);
