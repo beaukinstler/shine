70b3594bbd2a1f371ee887086945318be0ee1fe1
Beau Kinstler <beaukinstler@protonmail.com>
2017-09-24 02:55:09 +0000

    Add hard coded customers array to scope for testing

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,9 +1,33 @@
 var app = angular.module("customers",[]);
 
 var CustomerSearchController = function($scope){
+    $scope.customers = [];
     $scope.search = function(searchTerm){
-        $scope.searchedFor = searchTerm;
+        $scope.customers = [
+            {
+                "first_name":"Schuyler",
+                "last_name":"Cremin",
+                "email":"email1@example.com",
+                "username":"jilliano",
+                "created_at":"2015-03-04"
+            },
+            {
+                "first_name":"Derick",
+                "last_name":"Ebert",
+                "email":"email2@example.com",
+                "username":"badass",
+                "created_at":"2015-03-01"
+            },
+            {
+                "first_name":"Derick",
+                "last_name":"Johsons",
+                "email":"email3@example.com",
+                "username":"dungee",
+                "created_at":"2015-03-04"
+            }
+        ]
     }
 }
 
 app.controller("CustomerSearchController", ["$scope",CustomerSearchController]);
+
