c722b4cd69705b70817521c1171848d531c55e0c
Beau Kinstler <beaukinstler@protonmail.com>
2017-09-24 22:01:09 +0000

    Enable pagination with Angular

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,38 +1,49 @@
 var app = angular.module("customers",[]);
 
 var CustomerSearchController = function($scope,$http){
+    var page = 0;
     $scope.customers = [];
     $scope.search = function(searchTerm){
         $http.get("/customers.json",
-                    {"params":{"keywords":searchTerm}})
+                    {"params":{"keywords":searchTerm,"page":page}})
                     .then(function(response){
                         $scope.customers = response.data;
                     },function(response){
                         alert("There was a problem:" + response.status)
                     })
-        $scope.customers = [
-            {
-                "first_name":"Schuyler",
-                "last_name":"Cremin",
-                "email":"email1@example.com",
-                "username":"jilliano",
-                "created_at":"2015-03-04"
-            },
-            {
-                "first_name":"Derick",
-                "last_name":"Ebert",
-                "email":"email2@example.com",
-                "username":"badass",
-                "created_at":"2015-03-01"
-            },
-            {
-                "first_name":"Derick",
-                "last_name":"Johsons",
-                "email":"email3@example.com",
-                "username":"dungee",
-                "created_at":"2015-03-04"
-            }
-        ]
+        // $scope.customers = [
+        //     {
+        //         "first_name":"Schuyler",
+        //         "last_name":"Cremin",
+        //         "email":"email1@example.com",
+        //         "username":"jilliano",
+        //         "created_at":"2015-03-04"
+        //     },
+        //     {
+        //         "first_name":"Derick",
+        //         "last_name":"Ebert",
+        //         "email":"email2@example.com",
+        //         "username":"badass",
+        //         "created_at":"2015-03-01"
+        //     },
+        //     {
+        //         "first_name":"Derick",
+        //         "last_name":"Johsons",
+        //         "email":"email3@example.com",
+        //         "username":"dungee",
+        //         "created_at":"2015-03-04"
+        //     }
+        // ]
+    }
+    $scope.previousPage = function() {
+      if (page > 0) {
+        page = page - 1;
+        $scope.search($scope.keywords);
+      }
+    }
+    $scope.nextPage = function() {
+      page = page + 1;
+      $scope.search($scope.keywords);
     }
 }
 
