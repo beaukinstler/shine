a3ad5a240026cf5f0e4f65b6f88360fc33eadae3
Beau Kinstler <beaukinstler@protonmail.com>
2017-09-25 20:29:18 +0000

    Change the user test to use a custom matcher instead

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
+require 'support/violate_check_constraint_matcher'
 
 describe User do
   describe "email" do
     let(:user){
       User.create!(email: "foo@beaukinstler.info",password: "qwertyuiop")
     }
-    it "absolutely prevents invalid email addresses" do
+    it "absolutely prevents invalid email domain names" do
       expect {
         user.update_attribute(:email, "foo@bar.com")
-      }.to raise_error(ActiveRecord::StatementInvalid,
-      /email_must_be_company_email/i)
+      }.to violate_check_constraint(:email_must_be_company_email)
     end
   end
 end
